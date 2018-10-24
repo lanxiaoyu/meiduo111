8f99634c4b7ee774427480c5c0ae3ba71f125ede
python <18676663018@163.com>
2018-10-24 20:21:24 +0800

    统计用户名是否存在

--- meiduo111/apps/users/urls.py
+++ meiduo111/apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-
+   url(r'^usernames/(?P<username>\w{5,20})/count/$',views.UsernameCountView.as_view())
    ]
---
+++ meiduo111/apps/users/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
+
+
+class UsernameCountView(APIView):
+    def get(self,request,username):
+        """查询用户名个数"""
+        count = User.objects.filter(username=username).count()
+
+        #响应
+        return Response({
+            'username':username,
+            'count':count
+
+        })
