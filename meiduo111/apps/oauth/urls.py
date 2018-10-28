7bd466015a8ac162093a8ce85861f208a69164aa
python <18676663018@163.com>
2018-10-28 11:30:24 +0800

    获取openid

--- meiduo111/apps/oauth/urls.py
+++ meiduo111/apps/oauth/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url,include
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
+    url('^qq/authorization/$',views.QQurlView.as_view())
    ]
--- meiduo111/apps/oauth/views.py
+++ meiduo111/apps/oauth/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .qq_sdk import OAuthQQ
 
-# Create your views here.
+
+class QQurlView(APIView):
+    def get(self,request):
+        #接收登录后的地址
+        state = request.query_params.get('next')
+        #创建工具类对象
+        oauthqq= OAuthQQ(state = state)
+        #获取授权地址
+        url=oauthqq.get_qq_login_url()
+        #响应
+        return Response({
+            'login_url':url
+        })
+
+class QQLoginView(APIView):
+    def get(self,request):
+        #获取code
+        code=request.query_params.get('get')
+
+        #根据code获取token
+        oauthqq = OAuthQQ()
+        token = oauthqq.get_access_token(code)
+
+        #根据token获取openid
+        openid = oauthqq.get_openid(token)
+
+        #查询openid是否存在,如果存在则状态保持,登录成功
+        #如果不存在,则通知客户端转到绑定页面
+        pass
